901cdce3afd0dffa7bdda8f299c280ec752b521e
NotAlxT <anhntran56@gmail.com>
2023-07-20 11:41:18 -0400

    commit
    commit

---
+++ spotify-clone/app/layout.tsx
@@ -0,0 +1,28 @@
+import Sidebar from '@/components/Sidebar'
+import './globals.css'
+import type { Metadata } from 'next'
+import { Figtree } from 'next/font/google'
+
+const font = Figtree({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Spotify Clone',
+  description: 'Music Player',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body
+        className={font.className}>
+        <Sidebar>
+          {children}
+        </Sidebar>
+      </body>
+    </html>
+  )
+}
---
+++ spotify-clone/components/Header.tsx
@@ -0,0 +1,17 @@
+interface HeaderProps {
+    children: React.ReactNode;
+    className?: string; 
+}
+
+const Header: React.FC<HeaderProps>  = ({
+    children,
+    className
+}) => {
+    return ( 
+        <div>
+
+        </div>
+     );
+}
+ 
+export default Header;
