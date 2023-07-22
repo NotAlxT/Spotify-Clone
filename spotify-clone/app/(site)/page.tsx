3ca635463715c8f19fde5232fdb32d961b1300b1
NotAlxT <anhntran56@gmail.com>
2023-07-22 13:58:55 -0400

    commit
    commit update

--- spotify-clone/app/(site)/page.tsx
+++ spotify-clone/app/(site)/page.tsx
@@ -1,4 +1,5 @@
 import Header from "@/components/Header"
+import ListItem from "@/components/ListItem"
 
 export default function Home() {
   return (
@@ -11,7 +12,30 @@ export default function Home() {
     overflow-y-auto
     ">
       <Header>
-        Header
+        <div className="mb-2">
+          <h1 className="
+          text-white
+          text-3xl
+          font-semibold
+          ">
+            Welcome Back
+          </h1>
+          <div className="
+          grid
+          grid-cols-1
+          sm:grid-cols-2
+          xl:grid-cols-3
+          2xl:grid-cols-4
+          gap-3
+          mt-4
+          ">
+            <ListItem 
+              image='/img/liked.png'
+              name="Liked Songs"
+              href="liked"
+            />
+          </div>
+        </div>
       </Header>
     </div>
   )
---
+++ spotify-clone/components/ListItem.tsx
@@ -0,0 +1,56 @@
+'use client'
+
+import { useRouter } from "next/navigation"
+import Image from "next/image"
+
+interface ListItemProps {
+    image: string
+    name: string
+    href: string
+}
+
+const ListItem: React.FC<ListItemProps> = ({
+    image,
+    name,
+    href
+}) => {
+    const router = useRouter()
+
+    const onClick = () => {
+        router.push(href)
+    }
+
+    return ( 
+        <button className="
+        relative
+        group
+        flex
+        items-center
+        rounded-md
+        overflow-hidden
+        gap-x-4
+        bg-neutral-100/10
+        hover:bg-neutral-100/20
+        transition
+        pr-4
+        ">
+            <div className="
+            relative
+            min-h-[64px]
+            min-w-[64px]
+            ">
+                <Image 
+                className="object-cover"
+                fill
+                src={image}
+                alt="Image"
+                />
+            </div>
+            <p>
+                {name}
+            </p>
+        </button>
+     );
+}
+ 
+export default ListItem;
