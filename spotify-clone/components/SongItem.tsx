2cba04121c1020d1878b581094f354caabe37cc3
NotAlxT <anhntran56@gmail.com>
2023-07-30 11:48:15 -0400

    commit
    commit

---
+++ spotify-clone/components/SongItem.tsx
@@ -0,0 +1,54 @@
+'use client'
+
+import useLoadImage from "@/hooks/useLoadImage";
+import { Song } from "@/types";
+import Image from "next/image";
+
+interface SongItemProps {
+    data:Song
+    onClick: (id: string) => void
+}
+
+const SongItem: React.FC<SongItemProps> = ({
+    data,
+    onClick
+}) => {
+    const imagePath = useLoadImage(data)
+
+    return ( 
+        <div
+        onClick={() => onClick(data.id)}
+        className="
+        relative
+        group
+        flex
+        flex-col
+        items-center
+        justify-center
+        rounded-md
+        overflow-hidden
+        gap-x-4
+        bg-neutral-400/10
+        transition
+        p-3
+        ">
+            <div className="
+            relative
+            aspect-square
+            w-full
+            h-full
+            rounded-md
+            overflow-hidden
+            ">
+                <Image 
+                className="object-cover"
+                src={imagePath || '/img/liked.png'}
+                fill
+                alt='Image'
+                />
+            </div>
+        </div>
+     );
+}
+ 
+export default SongItem
--- spotify-clone/components/UploadModal.tsx
+++ spotify-clone/components/UploadModal.tsx
@@ -4,10 +4,18 @@ import { FieldValues, useForm, SubmitHandler } from 'react-hook-form';
 import { useState } from 'react';
 import Input from './Input';
 import Button from './Button';
+import { toast } from 'react-hot-toast';
+import { useUser } from '@/hooks/useUser';
+import uniqid from 'uniqid'
+import { useSupabaseClient } from '@supabase/auth-helpers-react';
+import { useRouter } from 'next/navigation';
 
 const UploadModal = () => {
-    const [isLoading, setIsLoading] = useState()
+    const [isLoading, setIsLoading] = useState(false)
     const uploadModal = useUploadModal()
+    const { user } = useUser()
+    const supabaseClient = useSupabaseClient()
+    const router = useRouter()
 
     const {
         register,
@@ -30,7 +38,82 @@ const UploadModal = () => {
     }
 
     const onSubmit: SubmitHandler<FieldValues> = async (values) => {
+        try {
+            setIsLoading(true)
 
+            const imageFile = values.image?.[0]
+            const songFile = values.song?.[0]
+
+            if (!imageFile || !songFile || !user) {
+                toast.error('Missing Fields')
+                return
+            }
+
+            const uniqueID = uniqid()
+
+            //Upload Song
+
+            const {
+                data: songData,
+                error: songError,
+            } = await supabaseClient
+            .storage
+            .from('songs')
+            .upload(`song-${values.title}-${uniqueID}`, songFile, {
+                cacheControl: '3600',
+                upsert: false
+            })
+
+            if (songError) {
+                setIsLoading(false)
+                return toast.error('Failed song upload.')
+            }
+            //Upload Img
+
+            const {
+                data: imageData,
+                error: imageError,
+            } = await supabaseClient
+            .storage
+            .from('images')
+            .upload(`image-${values.title}-${uniqueID}`, imageFile, {
+                cacheControl: '3600',
+                upsert: false
+            })
+
+            if(imageError) {
+                setIsLoading(false)
+                return toast.error('Failed Image Upload')
+            }
+
+            const {
+                error: supabaseError
+            } = await supabaseClient
+            .from('songs')
+            .insert({
+                user_id: user.id,
+                title: values.title,
+                author: values.author,
+                image_path: imageData.path,
+                song_path: songData.path
+            })
+
+            if(supabaseError) {
+                setIsLoading(false)
+                return toast.error(supabaseError.message)
+            }
+
+            router.refresh()
+            setIsLoading(false)
+            toast.success('Song Created')
+            reset()
+            uploadModal.onClose()
+
+        } catch (error) {
+            toast.error("Something went wrong")
+        } finally {
+            setIsLoading(false)
+        }
     }
     return (
         <Modal
@@ -61,7 +144,7 @@ const UploadModal = () => {
                         id='song'
                         type='file'
                         disabled={isLoading}
-                        accept='.mp3 .mp4'
+                        accept='.mp3'
                         {...register('song', { required: true })}
                     />
                 </div>
