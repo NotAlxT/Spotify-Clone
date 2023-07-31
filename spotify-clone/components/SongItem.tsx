50da716486d3760d60db4da34b0f54e4d388bad1
NotAlxT <anhntran56@gmail.com>
2023-07-31 00:33:35 -0400

    commit
    commit

--- spotify-clone/components/SongItem.tsx
+++ spotify-clone/components/SongItem.tsx
@@ -3,6 +3,7 @@
 import useLoadImage from "@/hooks/useLoadImage";
 import { Song } from "@/types";
 import Image from "next/image";
+import PlayButton from "./PlayButton";
 
 interface SongItemProps {
     data:Song
@@ -28,7 +29,9 @@ const SongItem: React.FC<SongItemProps> = ({
         rounded-md
         overflow-hidden
         gap-x-4
-        bg-neutral-400/10
+        cursor-pointer 
+        hover:bg-neutral-400/10 
+        bg-neutral-400/5
         transition
         p-3
         ">
@@ -46,6 +49,17 @@ const SongItem: React.FC<SongItemProps> = ({
                 fill
                 alt='Image'
                 />
+            </div>
+            <div className="flex flex-col items-start w-full pt-4 gap-y-1">
+                <p className="font-semibold truncate w-full">
+                    {data.title}
+                </p>
+                <p className="text-neutral-400 text-sm pb-4 w-full truncate">
+                    By {data.author}
+                </p>
+            </div>
+            <div className="absolute bottom-24 right-5">
+                <PlayButton />
             </div>
         </div>
      );
