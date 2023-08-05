c8a6b33fb5f2ed56a010ac76a82dc9c2a4df76a7
NotAlxT <anhntran56@gmail.com>
2023-08-05 15:34:41 -0400

    c
    c

--- spotify-clone/middleware.ts
+++ spotify-clone/middleware.ts
@@ -1,12 +1,9 @@
-import { createMiddlewareClient } from "@supabase/auth-helpers-nextjs";
-import { NextResponse, NextRequest } from "next/server";
+import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs'
+import { NextRequest, NextResponse } from 'next/server'
 
 export async function middleware(req: NextRequest) {
-    const res = NextResponse.next()
-    const supabase = createMiddlewareClient({
-        req,
-        res
-    })
-    await supabase.auth.getSession()
-    return res
-}
+  const res = NextResponse.next();
+  const supabase = createMiddlewareClient({ req, res });
+  await supabase.auth.getSession();
+  return res;
+};
